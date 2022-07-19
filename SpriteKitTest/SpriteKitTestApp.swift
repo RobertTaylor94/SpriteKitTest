7a00c69efb7ac598c975ddaa482dcbec21efcd7c
Robert Taylor <rjt903@gmail.com>
2022-07-19 14:25:15 +0100

    Initial Commit

---
+++ SpriteKitTest/SpriteKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitTestApp.swift
+//  SpriteKitTest
+//
+//  Created by Robert on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
